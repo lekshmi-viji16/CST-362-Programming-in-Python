b985e0af95df8263d5a91cac4b80f332d398586c
lekshmi-viji16 <vijilekshmi033@gmail.com>
2026-01-03 11:12:16 -0800

    Create swap_temp.py

---
+++ Module 1/Sequential problems/swap_temp.py
@@ -0,0 +1,17 @@
+a=int(input("enter first number:"))
+b=int(input("enter second number:"))
+temp=a
+a=b
+b=temp
+print("after swapping:")
+print("a=",a)
+print("b=",b)
+
+"""
+output:
+enter first number:20
+enter second number:30
+after swapping:
+a= 30
+b= 20
+"""
