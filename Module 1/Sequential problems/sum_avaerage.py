32f2131573df2d8fb3e491601ad746f104852eae
lekshmi-viji16 <vijilekshmi033@gmail.com>
2026-01-03 09:14:28 -0800

    Create sum_avaerage.py

---
+++ Module 1/Sequential problems/sum_avaerage.py
@@ -0,0 +1,16 @@
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+c=int(input("Enter third number:"))
+sum=a+b+c
+avg=sum/3
+print("Sum=",sum)
+print("Average=",avg)
+
+"""
+Output:
+Enter first number:2
+Enter second number:4
+Enter third number:6
+Sum= 12     
+Average= 4.0
+"""
