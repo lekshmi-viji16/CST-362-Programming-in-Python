d6ad0724249684f25061bc30934e8624185c15fb
lekshmi-viji16 <vijilekshmi033@gmail.com>
2026-01-03 11:28:01 -0800

    Create simple_interest.py

---
+++ Module 1/Sequential problems/simple_interest.py
@@ -0,0 +1,14 @@
+
+p = float(input("Enter the amount: "))
+r = float(input("Enter rate of interest: "))
+t = float(input("Enter time: "))
+si = (p*r*t)/100
+print("Simple Interest =", si)
+
+"""
+output:
+Enter the amount: 10000
+Enter rate of interest: 5
+Enter time: 2
+Simple Interest = 1000.0
+"""
