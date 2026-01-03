654bdee04fd23f5902912db28e40c4e0c9c28cb8
lekshmi-viji16 <vijilekshmi033@gmail.com>
2026-01-03 09:04:33 -0800

    Create arithmetic_operation.py

---
+++ Module 1/Sequential problems/arithmetic_operation.py
@@ -0,0 +1,18 @@
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+print("Sum=",a+b)
+print("Difference=",a-b)
+print("Product=",a*b)
+print("Quotient=",a/b)
+print("Remainder=",a%b)
+
+"""
+Output:
+Enter first number: 10
+Enter second number: 5
+Sum= 15
+Difference= 5
+Product= 50
+Quotient= 2.0
+Remainder= 0
+"""
