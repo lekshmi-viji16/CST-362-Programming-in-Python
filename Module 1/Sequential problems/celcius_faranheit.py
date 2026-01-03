07b3abc8ef54b1f9d7f84eaff52c59db65fce4eb
lekshmi-viji16 <vijilekshmi033@gmail.com>
2026-01-03 11:22:06 -0800

    Create celcius_faranheit.py

---
+++ Module 1/Sequential problems/celcius_faranheit.py
@@ -0,0 +1,10 @@
+c = float(input("Enter temperature in Celsius: "))
+f = (9/5)*c+32
+
+print("Temperature in Fahrenheit =", f)
+
+"""
+output:
+Enter temperature in Celsius: 0
+Temperature in Fahrenheit = 32.0
+"""
