c9b7c2aec4a8cdf696c361c6c1220aad3b12eb09
lekshmi-viji16 <vijilekshmi033@gmail.com>
2026-01-04 06:29:45 -0800

    Create largest_two.py

---
+++ Module 1/Decision Problems/largest_two.py
@@ -0,0 +1,13 @@
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+if a > b:
+    print("Largest:", a)
+else:
+    print("Largest:", b)
+
+"""
+Enter first number: 27
+Enter second number: 32
+Largest: 32
+"""
