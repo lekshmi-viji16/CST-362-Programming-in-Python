18a11adbbbabc72831769677118dcab0e075db80
lekshmi-viji16 <vijilekshmi033@gmail.com>
2026-01-04 03:18:55 -0800

    Create complete_divisible.py

---
+++ Module 1/Decision Problems/complete_divisible.py
@@ -0,0 +1,13 @@
+a=int(input("enter first number:"))
+b=int(input("enter second number"))
+if a%b==0:
+    print("completely divisible")
+else:
+    print("not divisible")
+
+"""
+output:
+enter first number:10
+enter second number2
+completely divisible
+"""
