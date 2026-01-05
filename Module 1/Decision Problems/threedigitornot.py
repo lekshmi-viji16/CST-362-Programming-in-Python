8ef25026b0527524505e04f5710cad892335e634
lekshmi-viji16 <vijilekshmi033@gmail.com>
2026-01-05 08:19:21 -0800

    Create threedigitornot.py

---
+++ Module 1/Decision Problems/threedigitornot.py
@@ -0,0 +1,11 @@
+n = int(input("Enter a number: "))
+if 100 <= abs(n) <= 999:
+    print("3-digit number")
+else:
+    print("Not a 3-digit number")
+
+"""
+output:
+Enter a number: 234
+3-digit number
+"""
