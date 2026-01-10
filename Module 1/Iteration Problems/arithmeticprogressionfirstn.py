d6e483b7432682b8c3e3b06fcaec4789fc93b403
lekshmi-viji16 <vijilekshmi033@gmail.com>
2026-01-10 06:27:28 -0800

    Create arithmeticprogressionfirstn.py

---
+++ Module 1/Iteration Problems/arithmeticprogressionfirstn.py
@@ -0,0 +1,32 @@
+a = int(input("Enter first term: "))
+d = int(input("Enter difference: "))
+n = int(input("Enter n: "))
+
+for i in range(n):
+    print(a + i*d)
+"""
+output:
+Enter first term: 1
+Enter difference: 5
+Enter n: 20
+1
+6
+11
+16
+21
+26
+31
+36
+41
+46
+51
+56
+61
+66
+71
+76
+81
+86
+91
+96
+"""
